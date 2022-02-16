c5d4ed5e6a30fde4ac4b4adda8a058a17dc19138
Dmitry <strigalov1991@mail.ru>
2022-02-16 19:20:45 +0300

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    cout << "My name is Dmitry" << endl;
+    cout << "I am 20 years old" << endl;
+    cout << "I live in Stavropol" << endl;
+    cout << "Thank,you" << endl;
+    cout << "Bye" << endl;
+    return 0;
+}
